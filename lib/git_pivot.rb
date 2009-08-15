11e13b4170a5f30ed0644fba5920591e55601581
Terence Lee <hone02@gmail.com>
2009-08-15 18:24:24 -0500

    skeleton app and ability to look at the current sprint stories

---
+++ lib/git_pivot.rb
@@ -0,0 +1,50 @@
+=begin
+  Copyright (c) 2009 Terence Lee.
+
+  This file is part of GitPivot
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+require 'ruport'
+
+class GitPivot
+
+  # ssl should default to yes since http basic auth is insecure
+  def initialize(project_id, token, use_ssl = true)
+    @tracker = PivotalTracker.new(project_id, token, {:use_ssl => use_ssl })
+  end
+
+  # list stories in current sprint
+  def current_sprint
+    iteration = @tracker.current_iteration
+    data = iteration.stories.collect do |story| 
+      [story.id, story.story_type, story.name]
+    end
+
+    puts Table(:data => data, :column_names => ["ID", "Type", "Name"])
+  end
+
+  # start story
+  def start_story
+  end
+
+  # finish story
+  def finish_story
+  end
+
+  # find story
+  def find_story
+  end
+end
